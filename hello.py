e3861d2304021e25cd403214711484b24a5041fc
Yash <yash@gmail.com>
2025-09-12 11:40:29 +0530

    Added python files in python branch

---
+++ hello.py
@@ -0,0 +1,2 @@
+# This is hello file 
+print("This file is in Python branch which is going to be solve real world problems")
---
+++ main.py
@@ -0,0 +1,6 @@
+# This is main.py file
+
+import os
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plts
